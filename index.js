18de0c3d0d9faae9d5974f5e365adc1c2cc3db12
heicx <heicx_sudo@163.com>
2015-06-05 01:46:48 +0800

    update views

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ $(function() {
 			}
 		},
 		toggle: function() {
-			this.save(done: !this.get("done"));
+			this.save({done: !this.get("done")});
 		}
 	});
 
-	var NoteList ＝ Backbone.Collection.extend({
+	var NoteList = Backbone.Collection.extend({
 		model: Note,
-		localStorage: new Backbone.localStorage("_items"),
+		localStorage: new Backbone.LocalStorage("_items"),
 		done: function() {
 			return this.where({done: true});
 		},
@@ -43,6 +43,36 @@ $(function() {
 		initialize: function() {
 			this.listenTo(this.model, "change", this.render);
 			this.listenTo(this.model, "destroy", this.remove);
+		},
+		render: function() {
+			this.$el.html(this.template(this.model.toJSON()));
+			this.$el.toogleDone("done", this.model.get("done"));
+			this.input = $(".edit");
+			return this;
+		},
+		toogleDone: function() {
+			this.model.toggle();
+		},
+		edit: function() {
+			this.$el.addClass("editing");
+			this.input.focus();
+		},
+		updateOnEnter: function(e) {
+			if(e.keyCode == 13) this.close();
+		},
+		close: function() {
+			var value = this.input.val();
+			if(!value) {
+				this.clear();
+			}else {
+				this.model.save({title: value});
+				this.$el.removeClass("editing");
+			}
+		},
+		clear: function() {
+			this.model.destroy();
 		}
 	});
+
+	var noteview = new NoteView;
 })
